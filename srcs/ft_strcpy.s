95e8c8e035fdd9e9eb2aa62e2de5d24a1ffbcb60
urabexon <hiroki708.a48uk@gmail.com>
2025-05-11 20:52:26 +0900

    strcpy done

--- srcs/ft_strcpy.s
+++ srcs/ft_strcpy.s
@@ -6,7 +6,25 @@
 #    By: urabex <urabex@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/05/11 19:20:59 by urabex            #+#    #+#              #
-#    Updated: 2025/05/11 19:21:00 by urabex           ###   ########.fr        #
+#    Updated: 2025/05/11 20:51:10 by urabex           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+; ft_strcpy.s
+
+section .text
+global ft_strcpy
+
+ft_strcpy:
+    push    rdi         ; 戻り値用,popしたものを保存
+
+.loop:
+    mov     al, [rsi]
+    mov     [rdi], al
+    inc     rsi
+    inc     rdi
+    test    al, al
+    jne     .loop
+
+    pop     rax         ; 元のdstを rax に戻して返す
+    ret
