94471267d058cc17e956169f14fbfc29df9874bf
urabexon <hiroki708.a48uk@gmail.com>
2025-05-11 20:40:04 +0900

    strlen done

--- srcs/ft_strlen.s
+++ srcs/ft_strlen.s
@@ -6,7 +6,24 @@
 #    By: urabex <urabex@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/05/11 19:21:17 by urabex            #+#    #+#              #
-#    Updated: 2025/05/11 19:21:18 by urabex           ###   ########.fr        #
+#    Updated: 2025/05/11 20:37:33 by urabex           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+; ft_strlen.s
+
+section .text
+global ft_strlen
+
+ft_strlen:
+    xor     rax, rax
+    
+.loop:
+    cmp     byte [rdi + rax], 0
+    je      .end
+
+    inc     rax
+    jmp     .loop
+
+.end:
+    ret
