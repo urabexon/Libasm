3a0802dad7f6b91f63b0231773c2e0f574ba5a4c
urabexon <hiroki708.a48uk@gmail.com>
2025-05-11 21:18:05 +0900

    write done

--- srcs/ft_write.s
+++ srcs/ft_write.s
@@ -6,7 +6,27 @@
 #    By: urabex <urabex@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/05/11 19:21:09 by urabex            #+#    #+#              #
-#    Updated: 2025/05/11 19:21:10 by urabex           ###   ########.fr        #
+#    Updated: 2025/05/11 21:16:14 by urabex           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+; ft_write.s
+
+section .text
+global ft_write
+extern __errno_location
+
+ft_write:
+    mov     rax, 1  ; syscall番号 (write)
+    syscall
+    cmp     rax, 0
+    jl      .error  ; rax < 0 → エラー
+    ret
+
+.error:
+    mov     rdi, rax
+    neg     rdi     ; 正の errnoコードに変換
+    call    __errno_location
+    mov     [rax], edi
+    mov     rax, -1
+    ret
