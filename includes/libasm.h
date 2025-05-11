7bd85394acb971f3d0ac66bd55324d94937547b3
urabexon <hiroki708.a48uk@gmail.com>
2025-05-11 19:40:53 +0900

    include file done

--- includes/libasm.h
+++ includes/libasm.h
@@ -6,7 +6,23 @@
 /*   By: urabex <urabex@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 18:45:00 by urabex            #+#    #+#             */
-/*   Updated: 2025/05/11 18:45:01 by urabex           ###   ########.fr       */
+/*   Updated: 2025/05/11 19:38:08 by urabex           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef LIBASM_H
+#define LIBASM_H
+
+// includes
+#include <stddef.h>
+#include <unistd.h>
+
+// prototype
+size_t  ft_strlen(const char *s);
+char    *ft_strcpy(char *dest, const char *src);
+int     ft_strcmp(const char *s1, const char *s2);
+ssize_t ft_write(int fd, const void *buf, size_t count);
+ssize_t ft_read(int fd, void *buf, size_t count);
+char    *ft_strdup(const char *s);
+
+#endif
